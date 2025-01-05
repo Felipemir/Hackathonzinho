e34046e7a344b3980e34aeaa48926a89503bfb7f
Robson Felipe <robsonfelipeprog@gmail.com>
2025-01-05 15:54:11 -0400

    adicionando o type module no back

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,22 @@
-// Importa o módulo Fastify
-const fastify = require("fastify")({ logger: true });
+// index.js
+import Fastify from 'fastify';
 
-// Define uma rota simples na URL raiz
-fastify.get("/", async (request, reply) => {
-  return { message: "Bem-vindo ao Fastify!" };
+const fastify = Fastify({ logger: true });
+
+// Define uma rota
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' };
 });
 
-// Inicia o servidor na porta 3000
-const start = async () => {
+// Inicia o servidor
+const startServer = async () => {
   try {
-    await fastify.listen({ port: 3000 });
-    console.log("Servidor rodando em http://localhost:3000");
+    const address = await fastify.listen({ port: 3000 });
+    console.log(`Servidor rodando em ${address}`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
   }
 };
 
-start();
+startServer();
